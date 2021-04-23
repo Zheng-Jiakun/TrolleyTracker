0a4a66b801f242b0d0df17e704d77103b8908cf0
Zheng-Jiakun <jzhengaq@ust.hk>
2021-04-23 12:54:19 +0800

    can compile, work with ses, but gcc has strange hardfault error

--- main.c
+++ main.c
@@ -1,161 +1,272 @@
-/**
- * Copyright (c) 2015 - 2020, Nordic Semiconductor ASA
+/***************************************************************************************/
+/*
+ * beacon_scanner
+ * Created by Manuel Montenegro, Sep 7, 2018.
  *
- * All rights reserved.
+ *  This is a Bluetooth 5 scanner. This code reads every advertisement from beacons
+ *  and sends its data through serial port.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- * @defgroup blinky_example_main main.c
- * @{
- * @ingroup blinky_example_freertos
- *
- * @brief Blinky FreeRTOS Example Application main file.
- *
- * This file contains the source code for a sample application using FreeRTOS to blink LEDs.
- *
- */
+ *  This code has been developed for Nordic Semiconductor nRF52840 PDK.
+*/
+/***************************************************************************************/
 
-#include <stdbool.h>
 #include <stdint.h>
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-#include "bsp.h"
+#include <stdio.h>
+#include <string.h>
 #include "nordic_common.h"
-#include "nrf_drv_clock.h"
-#include "sdk_errors.h"
+#include "nrf_sdm.h"
+#include "ble.h"
+#include "ble_hci.h"
+#include "ble_db_discovery.h"
+#include "ble_srv_common.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
+#include "nrf_sdh_soc.h"
+#include "nrf_pwr_mgmt.h"
+#include "app_util.h"
 #include "app_error.h"
+#include "ble_dis_c.h"
+#include "ble_rscs_c.h"
+#include "app_util.h"
+#include "app_timer.h"
+#include "bsp_btn_ble.h"
+#include "peer_manager.h"
+#include "peer_manager_handler.h"
+#include "fds.h"
+#include "nrf_fstorage.h"
+#include "ble_conn_state.h"
+#include "nrf_ble_gatt.h"
+#include "nrf_pwr_mgmt.h"
+#include "nrf_ble_scan.h"
 
-#include "uart.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
-#if LEDS_NUMBER <= 2
-#error "Board is not equipped with enough amount of LEDs"
-#endif
 
-#define TASK_DELAY        200           /**< Task delay. Delays a LED0 task for 200 ms */
-#define TIMER_PERIOD      1000          /**< Timer period. LED1 timer will expire after 1000 ms */
+#define APP_BLE_CONN_CFG_TAG        1                                   /**< Tag that identifies the BLE configuration of the SoftDevice. */
+#define APP_BLE_OBSERVER_PRIO       3                                   /**< BLE observer priority of the application. There is no need to modify this value. */
+#define APP_SOC_OBSERVER_PRIO       1                                   /**< SoC observer priority of the application. There is no need to modify this value. */
 
-TaskHandle_t  led_toggle_task_handle;   /**< Reference to LED0 toggling FreeRTOS task. */
-TaskHandle_t  uart_task_handle;   /**< Reference to UART FreeRTOS task. */
-TimerHandle_t led_toggle_timer_handle;  /**< Reference to LED1 toggling FreeRTOS timer. */
+#define SCAN_INTERVAL               0x1320                              /**< Determines scan interval in units of 0.625 millisecond. */
+#define SCAN_WINDOW                 0x1320                              /**< Determines scan window in units of 0.625 millisecond. */
+#define SCAN_DURATION           	0x0000                              /**< Duration of the scanning (timeout) in units of 10 milliseconds. If set to 0x0000, scanning continues until it is explicitly disabled. */
 
-/**@brief LED0 task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
-static void led_toggle_task_function (void * pvParameter)
+NRF_BLE_SCAN_DEF(m_scan);                                   /**< Scanning Module instance. */
+
+static bool                  m_memory_access_in_progress;   /**< Flag to keep track of ongoing operations on persistent memory. */
+
+static ble_gap_scan_params_t m_scan_param =                 /**< Scan parameters requested for scanning and connection. */
 {
-    UNUSED_PARAMETER(pvParameter);
-    while (true)
+    .active        = 0x00,
+    .interval      = SCAN_INTERVAL,
+    .window        = SCAN_WINDOW,
+    .filter_policy = BLE_GAP_SCAN_FP_ACCEPT_ALL,
+    .timeout       = SCAN_DURATION,
+//    .scan_phys     = BLE_GAP_PHY_CODED,                                 // Choose only one of the following scan_phys
+    .scan_phys     = BLE_GAP_PHY_1MBPS,
+//    .scan_phys     = BLE_GAP_PHY_2MBPS,
+    .extended      = 1,
+};
+
+static void scan_start(void);
+
+
+/**@brief Function for handling BLE events.
+ *
+ * @param[in]   p_ble_evt   Bluetooth stack event.
+ * @param[in]   p_context   Unused.
+ */
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+{
+
+    switch (p_ble_evt->header.evt_id)
     {
-        bsp_board_led_invert(BSP_BOARD_LED_0);
+        case BLE_GAP_EVT_ADV_REPORT:
+        {
+            NRF_LOG_RAW_INFO ("\r\n-----------Beacon Found-----------\r\n");
+            NRF_LOG_RAW_HEXDUMP_INFO (m_scan.scan_buffer.p_data, m_scan.scan_buffer.len);
+            //NRF_LOG_RAW_INFO ("----------------------------------\r\n");
+        }
 
-        /* Delay a task for a given number of ticks */
-        vTaskDelay(TASK_DELAY);
-
-        /* Tasks must be implemented to never return... */
+        default:
+            break;
     }
 }
 
-/**@brief UART task entry function.
+
+/**
+ * @brief SoftDevice SoC event handler.
  *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
+ * @param[in] evt_id    SoC event.
+ * @param[in] p_context Context.
  */
-static void uart_task_function (void * pvParameter)
+static void soc_evt_handler(uint32_t evt_id, void * p_context)
 {
-    UNUSED_PARAMETER(pvParameter);
-
-    uart_task_setup();
-    
-    while (true)
+    switch (evt_id)
     {
-        uart_task_loop();
+        case NRF_EVT_FLASH_OPERATION_SUCCESS:
+        /* fall through */
+        case NRF_EVT_FLASH_OPERATION_ERROR:
 
-        /* Delay a task for a given number of ticks */
-        vTaskDelay(TASK_DELAY);
+            if (m_memory_access_in_progress)
+            {
+                m_memory_access_in_progress = false;
+                scan_start();
+            }
+            break;
 
-        /* Tasks must be implemented to never return... */
+        default:
+            // No implementation needed.
+            break;
     }
 }
 
-/**@brief The function to call when the LED1 FreeRTOS timer expires.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the timer.
- */
-static void led_toggle_timer_callback (void * pvParameter)
-{
-    UNUSED_PARAMETER(pvParameter);
-    bsp_board_led_invert(BSP_BOARD_LED_1);
-}
 
-int main(void)
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+  */
+static void ble_stack_init(void)
 {
     ret_code_t err_code;
 
-    /* Initialize clock driver for better time accuracy in FREERTOS */
-    err_code = nrf_drv_clock_init();
+    err_code = nrf_sdh_enable_request();
     APP_ERROR_CHECK(err_code);
 
-    /* Configure LED-pins as outputs */
-    bsp_board_init(BSP_INIT_LEDS);
+    // Configure the BLE stack using the default settings.
+    // Fetch the start address of the application RAM.
+    uint32_t ram_start = 0;
+    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
+    APP_ERROR_CHECK(err_code);
 
-    /* Create task for LED0 blinking with priority set to 2 */
-    UNUSED_VARIABLE(xTaskCreate(led_toggle_task_function, "LED0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &led_toggle_task_handle));
+    // Enable BLE stack.
+    err_code = nrf_sdh_ble_enable(&ram_start);
+    APP_ERROR_CHECK(err_code);
 
-    /* Create task for UART with priority set to 2 */
-    UNUSED_VARIABLE(xTaskCreate(uart_task_function, "UART", configMINIMAL_STACK_SIZE + 200, NULL, 2, &uart_task_handle));
+    // Register handlers for BLE and SoC events.
+    NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
+    NRF_SDH_SOC_OBSERVER(m_soc_observer, APP_SOC_OBSERVER_PRIO, soc_evt_handler, NULL);
+}
 
-    /* Start timer for LED1 blinking */
-    led_toggle_timer_handle = xTimerCreate( "LED1", TIMER_PERIOD, pdTRUE, NULL, led_toggle_timer_callback);
-    UNUSED_VARIABLE(xTimerStart(led_toggle_timer_handle, 0));
 
-    /* Activate deep sleep mode */
-    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
-
-    /* Start FreeRTOS scheduler. */
-    vTaskStartScheduler();
-
-    while (true)
+/**@brief Function for handling Scanning Module events.
+ */
+static void scan_evt_handler(scan_evt_t const * p_scan_evt)
+{
+    switch(p_scan_evt->scan_evt_id)
     {
-        /* FreeRTOS should not be here... FreeRTOS goes back to the start of stack
-         * in vTaskStartScheduler function. */
+        case NRF_BLE_SCAN_EVT_SCAN_TIMEOUT:
+        {
+            NRF_LOG_INFO("Scan timed out.");
+            scan_start();
+        } break;
+
+        default:
+          break;
     }
 }
 
-/**
- *@}
- **/
+
+/**@brief Function for initializing the scanning and setting the filters.
+ */
+static void scan_init(void)
+{
+    ret_code_t          err_code;
+    nrf_ble_scan_init_t init_scan;
+
+    memset(&init_scan, 0, sizeof(init_scan));
+
+    init_scan.connect_if_match = false;
+    init_scan.conn_cfg_tag     = APP_BLE_CONN_CFG_TAG;
+    init_scan.p_scan_param     = &m_scan_param;
+
+    err_code = nrf_ble_scan_init(&m_scan, &init_scan, scan_evt_handler);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for starting scanning.
+ */
+static void scan_start(void)
+{
+    ret_code_t err_code;
+
+    // If there is any pending write to flash, defer scanning until it completes.
+    if (nrf_fstorage_is_busy(NULL))
+    {
+        m_memory_access_in_progress = true;
+        return;
+    }
+
+    err_code = nrf_ble_scan_start(&m_scan);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for initializing logging. */
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+
+/**@brief Function for initializing the timer. */
+static void timer_init(void)
+{
+    ret_code_t err_code = app_timer_init();
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for initializing power management.
+ */
+static void power_management_init(void)
+{
+    ret_code_t err_code;
+    err_code = nrf_pwr_mgmt_init();
+    APP_ERROR_CHECK(err_code);
+}
+
+
+/**@brief Function for handling the idle state (main loop).
+ *
+ * @details Handles any pending log operations, then sleeps until the next event occurs.
+ */
+static void idle_state_handle(void)
+{
+    if (NRF_LOG_PROCESS() == false)
+    {
+        nrf_pwr_mgmt_run();
+    }
+}
+
+
+
+int main(void)
+{
+
+    // Initialize.
+    log_init();
+    timer_init();
+    power_management_init();
+    ble_stack_init();
+    scan_init();
+
+    // Start execution.
+    NRF_LOG_RAW_INFO(    " ----------------\r\n");
+    NRF_LOG_RAW_INFO(	 "| Beacon scanner |");
+    NRF_LOG_RAW_INFO("\r\n ----------------\r\n");
+    
+    scan_start();
+
+
+    // Enter main loop.
+    for (;;)
+    {
+        idle_state_handle();
+    }
+}
